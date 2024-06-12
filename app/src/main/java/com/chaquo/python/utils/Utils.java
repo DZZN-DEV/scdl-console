566878bb41d0f57af935ffcd252c596c6db48a5e
DZZN-DEV <93328813+DZZN-DEV@users.noreply.github.com>
2024-06-12 04:26:09 +0200

    Update Utils.java

--- app/src/main/java/com/chaquo/python/utils/Utils.java
+++ app/src/main/java/com/chaquo/python/utils/Utils.java
@@ -2,34 +2,42 @@ package com.chaquo.python.utils;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import java.io.File;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Utils {
 
-    /** Make this package easy to copy to other apps by avoiding direct "R" references. (It
-     * would be better to do this by distributing it along with its resources in an AAR, but
-     * Chaquopy doesn't support getting Python code from an AAR yet.) */
     public static int resId(Context context, String type, String name) {
         Resources resources = context.getResources();
         return resources.getIdentifier(name, type, context.getApplicationInfo().packageName);
     }
 
     // Methode zur Validierung der SoundCloud-URL
-    public static boolean isValidURL(String url) {  // Methode umbenannt und angepasst
+    public static boolean isValidURL(String url) {
         String regex = "^(https?:\\/\\/)?(www\\.)?(soundcloud\\.com)\\/([a-zA-Z0-9_-]+)\\/([a-zA-Z0-9_-]+)$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(url);
         return matcher.matches();
     }
 
-    // Methode, um den internen Speicherpfad für scdl zu erhalten
-    public static String getInternalStoragePath(Context context) {
-        File file = new File(context.getFilesDir(), "scdl");
-        if (!file.exists()) {
-            file.mkdirs();
+    // Methode, um den Pfad aus einer URI zu erhalten
+    public static String getPathFromUri(Context context, Uri uri) {
+        String path = null;
+        String[] projection = { MediaStore.MediaColumns.DATA };
+
+        Cursor cursor = context.getContentResolver().query(uri, projection, null, null, null);
+        if (cursor != null) {
+            cursor.moveToFirst();
+            int column_index = cursor.getColumnIndexOrThrow(projection[0]);
+            path = cursor.getString(column_index);
+            cursor.close();
         }
-        return file.getAbsolutePath();
+
+        return path;
     }
 }
