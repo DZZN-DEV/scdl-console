439d06992d3fd80cc5a18dde7c2cfcba3797ccd3
DZZN-DEV <93328813+DZZN-DEV@users.noreply.github.com>
2024-06-12 02:55:29 +0200

    Update MainActivity.java

--- app/src/main/java/com/chaquo/python/console/MainActivity.java
+++ app/src/main/java/com/chaquo/python/console/MainActivity.java
@@ -75,7 +75,7 @@ public class MainActivity extends PythonConsoleActivity {
 
         String maxTracks = "5"; // For example purposes, this could be dynamic
         String downloadFavorites = "true"; // For example purposes, this could be dynamic
-        // String authToken = "your_auth_token"; Replace with actual token if needed
+        String authToken = "your_auth_token"; // Replace with actual token if needed
         String downloadPath = Utils.getInternalStoragePath(this); // Use internal storage path
 
         new Thread(() -> {
