bf58b80a405773c690b76a3b26ca311a6cf0cdbc
DZZN-DEV <93328813+DZZN-DEV@users.noreply.github.com>
2024-06-12 04:11:40 +0200

    Update Utils.java

--- app/src/main/java/com/chaquo/python/utils/Utils.java
+++ app/src/main/java/com/chaquo/python/utils/Utils.java
@@ -1,31 +1,30 @@
 package com.chaquo.python.utils;
 
-import android.content.*;
-import android.content.res.*;
-import android.net.Uri;
-import android.os.Environment;
+import android.content.Context;
+import android.content.res.Resources;
 import java.io.File;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
 public class Utils {
+
     /** Make this package easy to copy to other apps by avoiding direct "R" references. (It
      * would be better to do this by distributing it along with its resources in an AAR, but
-     * Chaquopy doesn't support getting Python code from an AAR yet.)*/
+     * Chaquopy doesn't support getting Python code from an AAR yet.) */
     public static int resId(Context context, String type, String name) {
         Resources resources = context.getResources();
         return resources.getIdentifier(name, type, context.getApplicationInfo().packageName);
     }
-    // Method to validate the SoundCloud URL
-    public static boolean isValidURL(String url) {
+
+    // Methode zur Validierung der SoundCloud-URL
+    public static boolean isValidURL(String url) {  // Methode umbenannt und angepasst
         String regex = "^(https?:\\/\\/)?(www\\.)?(soundcloud\\.com)\\/([a-zA-Z0-9_-]+)\\/([a-zA-Z0-9_-]+)$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(url);
         return matcher.matches();
     }
 
-    // Method to get the internal storage path for scdl
+    // Methode, um den internen Speicherpfad für scdl zu erhalten
     public static String getInternalStoragePath(Context context) {
         File file = new File(context.getFilesDir(), "scdl");
         if (!file.exists()) {
